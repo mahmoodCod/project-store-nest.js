34b8ce249b06003f2500ec9fa40d8092e102c944
mahmoodCod <nmashoomy11@gmail.com>
2025-12-15 09:06:11 +0330

    feat: add user module with entity, DTOs and service
    - Create user module with controller and service
    - Add user entity for database
    - Add create and update user DTOs
    - Add @nestjs/mapped-types dependency
    - Register UserModule in app.module.ts

--- api-store-nest.js/src/app.module.ts
+++ api-store-nest.js/src/app.module.ts
@@ -3,22 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    })
-    ,TypeOrmModule.forRoot({
+    }),
+    TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
-      password:  process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
