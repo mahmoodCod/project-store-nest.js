4dfe5b36d13e0dc13ddbb0f28c8a73376f358e64
mahmoodCod <nmashoomy11@gmail.com>
2025-12-15 08:21:22 +0330

    feat: add ConfigModule for environment variables
    - Add @nestjs/config package
    - Configure ConfigModule as global module in app.module.ts
    - Update package-lock.json

--- api-store-nest.js/src/app.module.ts
+++ api-store-nest.js/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
+    ,TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
