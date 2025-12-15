de837794ce987651cba6239f9c6a62e8480dc650
mahmoodCod <nmashoomy11@gmail.com>
2025-12-15 12:27:33 +0330

    feat(user-controller): enhance create endpoint response format
    - Make create method async to handle service response
    - Add Response decorator and Express Response type
    - Return structured JSON response with status code and message

--- api-store-nest.js/src/user/user.controller.ts
+++ api-store-nest.js/src/user/user.controller.ts
@@ -6,18 +6,26 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    const createUser = await this.userService.create(createUserDto);
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: createUser,
+      message: 'User created successfully :))',
+    });
   }
 
   @Get()
