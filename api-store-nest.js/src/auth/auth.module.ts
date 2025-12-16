64b19fb8913c18054b422432691936a5f9bac3b0
mahmoodCod <nmashoomy11@gmail.com>
2025-12-16 20:43:06 +0330

    feat(auth-module): register AuthController in module
    - Add AuthController to controllers array in AuthModule

--- api-store-nest.js/src/auth/auth.module.ts
+++ api-store-nest.js/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { JwtStrategy } from './strateges/jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
     }),
   ],
+  controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
