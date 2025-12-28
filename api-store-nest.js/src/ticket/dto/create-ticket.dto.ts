bd784181b9b96c0ffe71fb2c75de80d4d4aeae4c
mahmoodCod <nmashoomy11@gmail.com>
2025-12-28 17:47:54 +0330

    feat(ticket-dto): add validation to CreateTicketDto
    - Add validation rules for title, subject, and description fields
    - Add IsString and IsNotEmpty validations for required fields

--- api-store-nest.js/src/ticket/dto/create-ticket.dto.ts
+++ api-store-nest.js/src/ticket/dto/create-ticket.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTicketDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTicketDto {
+  @IsString({ message: 'Title must be a string' })
+  @IsNotEmpty({ message: 'Title is required' })
+  title: string;
+
+  @IsString({ message: 'Subject must be a string' })
+  @IsNotEmpty({ message: 'Subject is required' })
+  subject: string;
+
+  @IsString({ message: 'Description must be a string' })
+  @IsNotEmpty({ message: 'Description is required' })
+  description: string;
+}
