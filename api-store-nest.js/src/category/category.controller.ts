ac77b812f038c4ce12801d50951bbdc9dd8e9e89
mahmoodCod <nmashoomy11@gmail.com>
2026-01-04 06:21:48 +0330

    refactor(category): remove unused endpoints and methods
    - Remove update, remove, and findOne endpoints from category controller
    - Remove unused Patch, Delete, Param decorators and UpdateCategoryDto import
    - Remove update, remove, and findOne methods from category service
    - Simplify controller imports to only used decorators

--- api-store-nest.js/src/category/category.controller.ts
+++ api-store-nest.js/src/category/category.controller.ts
@@ -1,17 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Res,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Res, HttpStatus } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 import { Response } from 'express';
 
 @Controller('category')
@@ -41,23 +30,5 @@ export class CategoryController {
       data: category,
       message: 'Category all successfully :))',
     });
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateCategoryDto: UpdateCategoryDto,
-  ) {
-    return this.categoryService.update(+id, updateCategoryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
   }
 }
--- api-store-nest.js/src/category/category.service.ts
+++ api-store-nest.js/src/category/category.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { Repository } from 'typeorm';
@@ -19,17 +18,5 @@ export class CategoryService {
 
   async findAll(): Promise<Category[]> {
     return await this.categoryRepository.find({ relations: ['product'] });
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
-
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
   }
 }
