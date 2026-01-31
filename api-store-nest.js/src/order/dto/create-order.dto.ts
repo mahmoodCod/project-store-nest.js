5814e9f7cfb4929f18ad08c393896f9459416660
mahmoodCod <nmashoomy11@gmail.com>
2026-01-31 16:13:58 +0330

    feat(order-dto): add custom validation messages to CreateOrderDto
    - Add message option to IsNumber, IsEnum, and IsDateString validators

--- api-store-nest.js/src/order/dto/create-order.dto.ts
+++ api-store-nest.js/src/order/dto/create-order.dto.ts
@@ -2,28 +2,30 @@ import { IsDateString, IsEnum, IsNumber, IsOptional } from 'class-validator';
 import { orderStatus } from '../enum/order-status.enum';
 
 export class CreateOrderDto {
-  @IsNumber()
+  @IsNumber({}, { message: 'userId must be a number' })
   userId: number;
 
-  @IsEnum(orderStatus)
+  @IsEnum(orderStatus, {
+    message: 'status must be one of the following values',
+  })
   @IsOptional()
   status?: orderStatus;
 
-  @IsDateString()
+  @IsDateString({}, { message: 'set_time must be a valid ISO date string' })
   @IsOptional()
   set_time?: Date;
 
-  @IsDateString()
+  @IsDateString({}, { message: 'payed_time must be a valid ISO date string' })
   @IsOptional()
   payed_time?: Date;
 
-  @IsNumber()
+  @IsNumber({}, { message: 'addressId must be a number' })
   addressId: number;
 
-  @IsNumber()
+  @IsNumber({}, { message: 'total_price must be a number' })
   total_price: number;
 
-  @IsNumber()
+  @IsNumber({}, { message: 'discount_code must be a number' })
   @IsOptional()
   discount_code?: number;
 }
