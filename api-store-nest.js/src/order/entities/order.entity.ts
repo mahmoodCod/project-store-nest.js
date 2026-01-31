18cf15e855770fff5d1c898ed2f33be876c28509
mahmoodCod <nmashoomy11@gmail.com>
2026-01-31 05:18:01 +0330

    feat(order-entity): define Order entity structure
    - Add id, user (ManyToOne to User), status enum, and set_time columns
    - Map entity to orders table and set JoinColumn for userId
    - Add status enum with pending, completed, cancelled values

--- api-store-nest.js/src/order/entities/order.entity.ts
+++ api-store-nest.js/src/order/entities/order.entity.ts
@@ -1,1 +1,31 @@
-export class Order {}
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.orders)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column({
+    type: 'enum',
+    enum: ['pending', 'compeleted', 'canselleded'],
+    default: 'pending',
+  })
+  status: string;
+
+  @Column({ type: 'timestamp', nullable: true })
+  set_time: Date;
+
+  @Column({ type: 'timestamp', nullable: true })
+
+}
