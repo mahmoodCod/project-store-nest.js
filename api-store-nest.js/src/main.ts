f2a644b16aa459b78d92d73325c87d1b3ff70b13
mahmoodCod <nmashoomy11@gmail.com>
2026-02-11 12:33:27 +0330

    fix(main): enable ResponceFormaterInterceptor globally
    - use useGlobalInterceptors instead of useGlobalPipes for ResponceFormaterInterceptor

--- api-store-nest.js/src/main.ts
+++ api-store-nest.js/src/main.ts
@@ -7,7 +7,7 @@ import { ResponceFormaterInterceptor } from './interceptors/responce-formater.in
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  // app.useGlobalPipes(new ResponceFormaterInterceptor());
+  app.useGlobalInterceptors(new ResponceFormaterInterceptor());
 
   // swagger
   const config = new DocumentBuilder()
