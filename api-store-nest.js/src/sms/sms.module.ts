c3d06557e6418417dbae8b51e706c73cc5dbbfbf
mahmoodCod <nmashoomy11@gmail.com>
2026-02-12 17:34:38 +0330

    feat(sms): add Bull queue for sending SMS
    - Add sms-queue in SmsModule
    - Implement sendMultiSms in SmsService with retries and delay
    - Add send-sms processor for queue jobs

--- api-store-nest.js/src/sms/sms.module.ts
+++ api-store-nest.js/src/sms/sms.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SmsController } from './sms.controller';
 import { SmsService } from './sms.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
+  imports: [BullModule.registerQueue({ name: 'sms-queue' })],
   controllers: [SmsController],
   providers: [SmsService],
 })
--- api-store-nest.js/src/sms/sms.service.ts
+++ api-store-nest.js/src/sms/sms.service.ts
@@ -1,4 +1,22 @@
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
 
 @Injectable()
-export class SmsService {}
+export class SmsService {
+  constructor(@InjectQueue('sms-queue') private smsQueue: Queue) {}
+
+  async sendMultiSms(number: string, text: string) {
+    await this.smsQueue.add(
+      'send-sms',
+      { number, text },
+      {
+        attempts: 3,
+        backoff: 5000,
+        delay: 2000,
+        removeOnComplete: true,
+        removeOnFail: false,
+      },
+    );
+  }
+}
